681060dd8d2fc288c1225a5c4dcc8ad8e82fcd76
tianhanxxx <tianhanxxx@gmail.com>
2014-07-22 10:50:45 +0800

    增加XML操作类，使用XML进行数据保存和读取
    Signed-off-by: tianhanxxx <tianhanxxx@gmail.com>

---
+++ recovery/recovery/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using xml;
+
+namespace recovery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_test_Click(object sender, EventArgs e)
+        {
+            ThintaXMLConfig.DefaultConfig["test", "test"] = "1";
+            ThintaXMLConfig.DefaultConfig.Save();
+        }
+    }
+}
